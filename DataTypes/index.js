4c909b723a9f5c666fbcd307d21ffc8a0a8c2ff0
Rigo-codes <rodrigo.s.m.jr@gmail.com>
2024-11-06 21:27:42 -0800

    pushhhhhhhhhit

---
+++ DataTypes/index.js
@@ -0,0 +1,48 @@
+const greeting = "Hello, World!";
+
+console.log(greeting);
+
+// example of a number in a string
+
+const myAgeAsAstring = "22";
+
+console.log(myAgeAsAstring);
+console.log(typeof myAgeAsAstring);
+
+// Number
+const myAge = 22;
+console.log(myAge);
+console.log(typeof myAge);
+
+// boolean
+const isStudent = true;
+const hasGraduated = false;
+
+console.log(isStudent);
+console.log(hasGraduated);
+
+// undefined
+
+// let user =;
+// console.log(user);
+
+// null
+
+const nullUser = null;
+console.log(nullUser);
+
+// Array
+
+const people = ["Alice", "Bob", "Charlie"];
+
+console.log(people);
+
+// Object
+
+const person = {
+    name: "Alice",
+    age: 22,
+    iStudent: false
+};
+
+console.log(person);
